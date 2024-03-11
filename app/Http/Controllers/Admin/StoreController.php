d77aaa5146c3f57ad0cb56e1ccdcde8e7082a5d4
Ygor-Machado <ygaounderground011@gmail.com>
2024-03-11 18:52:49 -0300

    feat: cria método destroy

--- app/Http/Controllers/Admin/StoreController.php
+++ app/Http/Controllers/Admin/StoreController.php
@@ -47,4 +47,12 @@ class StoreController extends Controller
 
         return $store;
     }
+
+    public function destroy($store)
+    {
+        $store = Store::find($store);
+        $store->delete();
+
+        return redirect('admin/stores');
+    }
 }
