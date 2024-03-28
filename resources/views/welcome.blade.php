62d6d2a0082e97d0879f10d06afcd32c80983afa
Ygor-Machado <ygaounderground011@gmail.com>
2024-03-28 17:07:34 -0300

    feat: imagens de produtos na single

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,18 @@
                 <div class="card" style="width: 98%;">
                     @if($product->photos->count())
                         <img src="{{ asset('storage/' . $product->photos->first()->image) }}" alt="">
+                    @else
+                        <img src="{{ asset('assets/img/noPhoto.jpg') }}" alt="">
                     @endif
                     <div class="card-body">
                         <h2 class="card-title">{{ $product->name }}</h2>
                         <p class="card-text">{{ $product->description }}</p>
+                        <h3>
+                            R$ {{ number_format($product->price, '2', ',', '.') }}
+                        </h3>
+                        <a href="{{ route('product.single', ['slug' => $product->slug]) }}" class="btn btn-success">
+                            Ver Produto
+                        </a>
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
 Route::get('/', 'HomeController@index' )->name('home');
-
+Route::get('/product/{slug}', 'HomeController@single')->name('product.single');
 
 Route::group(['middleware' => ['auth']], function() {
 
@@ -37,9 +25,6 @@ Route::group(['middleware' => ['auth']], function() {
 
     });
 });
-
-
-
 
 Auth::routes();
 
